3de598b7e768bb578c7c3dc5ecd708da75e8c2c3
Khongchai <world1955@hotmail.com>
2020-08-28 00:40:22 +0700

    first commit

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,10 +1,18 @@
+var recipients;
+var subject;
+var message;
+
 document.addEventListener('DOMContentLoaded', function() {
+  recipients = document.querySelector('#compose-recipients');
+  subject = document.querySelector('#compose-subject');
+  message = document.querySelector('#compose-body');
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener.onsubmit = send_email()
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -17,9 +25,9 @@ function compose_email() {
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  recipients.value = '';
+  subject.value = '';
+  message.value = '';
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +38,39 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email()
+{
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: 'baz@example.com',
+        subject: 'Meeting time',
+        body: 'How about we meet tomorrow at 3pm?'
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+  });
+  /* 
+  fetch('/emails', 
+  {
+    method: "POST",
+    body: JSON.stringify
+    ({
+      recipients: recipients.value,
+      subject: subject.value, 
+      body: message.value
+    }) //paren mistake here is preventing you from submitting to API
+    .then(response => response.json())
+    .then(result=>
+    {
+      //notify user that the email is sent successfully
+      alert(result);
+    })
+  });
+  */
 }
